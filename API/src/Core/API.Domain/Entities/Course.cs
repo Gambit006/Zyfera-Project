d6aa75501b5da41c7b0d72c5d79b18ee70d84fdd
ihsanndamar <111857789+ihsanndamar@users.noreply.github.com>
2024-05-06 23:05:05 +0300

    feat: entities were added

---
+++ API/src/Core/API.Domain/Entities/Course.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Domain.Entities
+{
+    public class Course
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+
+        public virtual ICollection<Student> Students { get; set; }
+    }
+}
---
+++ API/src/Core/API.Domain/Entities/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Domain.Entities
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Number { get; set; }
+
+        public virtual ICollection<Course> Courses { get; set; }
+    }
+}
