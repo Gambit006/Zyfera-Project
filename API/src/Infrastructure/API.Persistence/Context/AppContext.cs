53deb6fa4785d2bd195bcfacc38cb4c581ff1ea7
ihsanndamar <111857789+ihsanndamar@users.noreply.github.com>
2024-05-07 01:06:30 +0300

    feat: context was created

---
+++ API/src/Infrastructure/API.Persistence/Context/AppContext.cs
@@ -0,0 +1,47 @@
+﻿using API.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Persistence.Context
+{
+    public class AppContext : DbContext
+    {
+        public AppContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses{ get; set; }
+        public DbSet<StudentCourse> StudentCourses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                var connectionString = "";
+                optionsBuilder.UseSqlServer(connectionString, opt => { 
+                    opt.EnableRetryOnFailure(); 
+                });
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Student>().Property(s => s.Id).HasDefaultValue<Guid>();
+            modelBuilder.Entity<Course>().Property(c => c.Id).HasDefaultValue<Guid>();
+            modelBuilder.Entity<StudentCourse>().Property(sc => sc.Id).HasDefaultValue<Guid>();
+
+            modelBuilder.Entity<StudentCourse>().HasKey(sc => new { sc.StudentId, sc.CourseId });
+        }
+
+
+    }
+}
